49662ef57054714cbd2d720be1964de96f72113f
Oscar Pérez <omiguelperez@outlook.com>
2019-07-31 14:38:44 -0500

    Add missing file

---
+++ functions/components/controllers/posts/PostsController.js
@@ -0,0 +1,16 @@
+const { Notifications } = require('./../notifications/Notifications')
+const { Posts } = require('./Posts')
+
+exports.updatePost = function (dataSnapshot, context) {
+  const notifications = new Notifications()
+  
+  if (dataSnapshot.before.data().publicated === false &&
+    dataSnapshot.after.data().publicated === true) {
+      return notifications.sendNotification(
+        dataSnapshot.after.data().title,
+        dataSnapshot.after.data().description,
+        null,
+        ''
+      )
+    }
+}
